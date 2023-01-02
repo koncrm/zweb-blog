9bce3ec4c5574ab474aa9da376519328a9062636
koncrm <dev.mahmoud.adel@gmail.com>
2023-01-02 09:24:20 +0200

    add .nojekyll

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: 'static',
-  ssr: false,
-  nitro: {
-    preset: 'service-worker'
-  },
+  // target: 'static',
+  // ssr: false,
+  // nitro: {
+  //   preset: 'service-worker'
+  // },
   extends: 'content-wind',
   app: {
     baseURL: '/zweb-blog/'
