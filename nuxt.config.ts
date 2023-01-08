9a829157c9ab99d8ea9e390bbb01bfcb284ee5dc
koncrm <dev.mahmoud.adel@gmail.com>
2023-01-08 17:51:41 +0200

    Edit Nuxt Config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,5 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  // target: 'static',
-  // ssr: false,
-  // nitro: {
-  //   preset: 'service-worker'
-  // },
   extends: 'content-wind',
   app: {
     baseURL: 'wind.zwebcourses.com'
