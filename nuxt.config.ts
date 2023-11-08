1c491998c39b9c76bba24085c1abf548b7bc3480
zwebcourses <dev.mahmoud.adel@gmail.com>
2023-11-08 20:23:42 +0200

    change base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   extends: 'content-wind',
-  // app: {
-  //   baseURL: 'https://wind.zwebcourses.com/'
-  // }
+  app: {
+    baseURL: '/nuxt-wind-theme'
+  }
 })
